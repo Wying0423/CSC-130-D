105627f400e383404ba3666498f607e2de645e4e
Wying0423 <39095742+Wying0423@users.noreply.github.com>
2018-05-15 11:34:11 -0400

    CSC 130 Lab 02
    Lab 02 in CSC 130

---
+++ WyingLab02 (1).java
@@ -0,0 +1,22 @@
+//Wanghao Ying Lab02, Feb 6th 2018
+public class WyingLab02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("23 + 10 = ");//result for 33
+System.out.println(23 + 10 );
+
+System.out.print("23 - 10 = " );//result for 13
+System.out.println(23 - 10 );
+
+System.out.print("23 * 10 = ");//result for 230
+System.out.println(23 * 10 );
+
+System.out.print("23 / 10 = ");//result for 2 , remainder 3
+System.out.print(23 / 10);
+System.out.println(" (remainder "+23 % 10 +")");
+
+
+	}
+
+}
